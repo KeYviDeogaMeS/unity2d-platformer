d52ed833ae862d0c86253626d83abbe69c831e24
Alexander Nason <74044634+forTeemo@users.noreply.github.com>
2024-03-15 16:27:59 +0200

    jump improve

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
     [SerializeField, Range(0, 100f)] private float maxMoveSpeed = 4.0f;
     [SerializeField, Range(0, 100f)] private float maxJumpHeight = 4.0f;
+    [SerializeField, Range(0, 100f)] private float minJumpHeight = 1.5f;
+    [SerializeField, Range(0, 1)] private float jumpBufferingTime = 0.25f;
 
     private Rigidbody2D body = null;
     private CollisionCheck collisionCheck;
 
     private float horizontalDirection;
     private bool jump;
+
+    private float bufferingTime;
+    private float jumpStart;
+    private bool endJump;
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
@@ -21,23 +28,35 @@ public class MovementController : MonoBehaviour
     {
         horizontalDirection = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && collisionCheck.OnGround)
+        if (Input.GetButtonDown("Jump"))
         {
-            jump = true;
+            bufferingTime = jumpBufferingTime;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            endJump = true;
         }
     }
     private void FixedUpdate()
     {
         Vector2 velocity = body.velocity;
 
-        if (jump)
+        if (bufferingTime > 0 && collisionCheck.OnGround)
         {
             velocity.y = Mathf.Sqrt(2 * maxJumpHeight * Mathf.Abs(Physics2D.gravity.y) * body.gravityScale);
+            jumpStart = transform.position.y;
+        }
+
+        if (endJump && transform.position.y - jumpStart >= minJumpHeight)
+        {
+            velocity.y *= 0.5f;
+            endJump = false;
         }
 
         velocity.x = horizontalDirection * maxMoveSpeed;
 
         body.velocity = velocity;
-        jump = false;
+
+        bufferingTime -= Time.deltaTime;
     }
 }
